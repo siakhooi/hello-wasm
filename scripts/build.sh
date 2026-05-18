85ecedd4d747b2e89483d76bab562eadb4d73102
Siak Hooi <siakhooi@gmail.com>
2026-05-18 01:55:05 +0000

    add function call

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-if ! command -v emcc &> /dev/null; then
-    echo "Emscripten compiler (emcc) not found. Please install Emscripten SDK and ensure emcc is in your PATH."
-    exit 1
+if ! command -v emcc &>/dev/null; then
+  echo "Emscripten compiler (emcc) not found. Please install Emscripten SDK and ensure emcc is in your PATH."
+  exit 1
 fi
 
 mkdir -p docs
 
-emcc src/hello.c -O2 -o docs/hello.js
+emcc src/hello.c -O2 \
+  -s  EXPORTED_RUNTIME_METHODS='["ccall"]' \
+  -o docs/hello.js
--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <emscripten/emscripten.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, Wasm!\n");
     return 0;
+}
+
+
+EMSCRIPTEN_KEEPALIVE
+int doAddition(int a, int b) {
+    printf("Doing addition: %d + %d in wasm\n", a, b);
+    return a + b;
 }
